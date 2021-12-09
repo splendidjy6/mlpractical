e9fbfc34c9f8b16b31844f40e7ee585d876dc759
splendidjy6 <813134770@qq.com>
2021-12-09 06:54:24 +0000

    update

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -47,6 +47,9 @@ if args.block_type == 'conv_block':
 elif args.block_type == 'empty_block':
     processing_block_type = EmptyBlock
     dim_reduction_block_type = EmptyBlock
+elif args.block_type == 'BN':
+    processing_block_type = ConvolutionalProcessingBlock_bn
+    dim_reduction_block_type = ConvolutionalDimensionalityReductionBlock_bn
 else:
     raise ModuleNotFoundError
 
